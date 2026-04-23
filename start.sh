761d6774670cdda5c25e40dbefb00e60fafda54f
Gustin Lau <gustinlau@gmail.com>
2026-04-23 19:35:53 +0800

    feat: update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-pm2 start main.py --interpreter=.venv/bin/python --name daily_stock_analysis --no-autorestart
+pm2 start main.py --interpreter=.venv/bin/python --name daily_stock_analysis --no-autorestart --cron-restart="0 0 * * *"
